8b0003bfec3354e4fab38b06fbe88f522685df1d
Filip <f.grzywok@gmail.com>
2021-04-07 22:43:24 +0200

    feat: EventBus

--- euphonium/include/EventBus.h
+++ euphonium/include/EventBus.h
@@ -6,6 +6,7 @@
 #include <queue>
 #include <any>
 #include <map>
+#include <set>
 
 enum class EventType : uint32_t {
     EVENT_FETCH_SERVICES,
@@ -16,17 +17,21 @@ struct Event {
     std::any eventValue;
 };
 
-typedef std::function<void(std::unique_ptr<Event>)> eventListener;
+class EventSubscriber {
+public:
+    virtual ~EventSubscriber() = 0;
+    virtual void handleEvent(std::unique_ptr<Event> event) = 0;
+};
 
 class EventBus {
 private:
     std::queue<std::unique_ptr<Event>> eventQueue;
-    std::map<uint32_t, eventListener&> registeredListeners;
+    std::map<EventType, std::set<std::unique_ptr<EventSubscriber>>> registeredListeners;
 public:
     EventBus();
     void update();
     void postEvent(std::unique_ptr<Event> event);
-    void addListener(EventType eventType, eventListener& eventListener);
+    void addListener(EventType eventType, std::unique_ptr<EventSubscriber> eventSubscriber);
 };
 
 #endif
--- euphonium/src/EventBus.cpp
+++ euphonium/src/EventBus.cpp
@@ -1,17 +1,34 @@
 #include "EventBus.h"
 
-void EventBus() {
-
+EventBus::EventBus()
+{
+    // this->registeredListeners = std::map<EventType, std::set<eventListener*>>();
 }
 
-void EventBus::update() {
-
+void EventBus::update()
+{
+    while (!eventQueue.empty())
+    {
+        std::unique_ptr localPtr(std::move(eventQueue.front()));
+        eventQueue.pop();
+        auto type = localPtr->eventType;
+        auto it = registeredListeners.find(type);
+        if (it != registeredListeners.end())
+        {
+            for (auto &&subscriber : (*it).second)
+            {
+                subscriber->handleEvent(std::move(localPtr));
+            }
+        }
+    }
 }
 
-void EventBus::postEvent(std::unique_ptr<Event> event) {
-
+void EventBus::postEvent(std::unique_ptr<Event> event)
+{
+    eventQueue.push(std::move(event));
 }
 
-void EventBus::addListener(EventType eventType, eventListener& eventListener) {
-
+void EventBus::addListener(EventType eventType, std::unique_ptr<EventSubscriber> eventSubscriber)
+{
+    registeredListeners[eventType].insert(std::move(eventSubscriber));
 }
