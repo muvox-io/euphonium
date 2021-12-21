f018b917af18375258a6020ddbf0aa1638b9d12b
Filip <f.grzywok@gmail.com>
2021-12-21 10:18:29 +0100

    fix: Missing file in gh action

--- targets/esp32/generate_full_build.sh
+++ targets/esp32/generate_full_build.sh
@@ -8,7 +8,7 @@ cd ../../targets/esp32
 # package scripts
 cp ../../euphonium/scripts/scripts.tar scripts.tar
 
-tar -czvf euphonium-$EUPH_VER.tar.gz build/*.bin build/partition_table/partition-table.bin build/bootloader/bootloader.bin
+tar -czvf euphonium-$EUPH_VER.tar.gz build/*.bin build/partition_table/partition-table.bin build/bootloader/bootloader.bin flash_all.sh
 cp build/euphonium-esp32.bin ota.bin
 sha256sum ota.bin | cut -d " " -f 1 > ota_checksum.txt
 sha256sum scripts.tar | cut -d " " -f 1 > scripts_checksum.txt
