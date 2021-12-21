32201cfbd8fb42b01dd2c982842d173e3889c263
Filip <f.grzywok@gmail.com>
2021-12-21 12:36:39 +0100

    feat: Better build

--- targets/esp32/generate_full_build.sh
+++ targets/esp32/generate_full_build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 sh build_recovery.sh
 sh build_app.sh
-cd ../../euphonium/scripts
+cp -r ../../euphonium/scripts/. app/spiffs/
+cd app/spiffs
 tar -cvf scripts.tar *
-cd ../../targets/esp32
+cd ../../
 
 # package scripts
-cp ../../euphonium/scripts/scripts.tar scripts.tar
+cp app/spiffs/scripts.tar scripts.tar
 
 tar -czvf euphonium-$EUPH_VER.tar.gz build/*.bin build/partition_table/partition-table.bin build/bootloader/bootloader.bin flash_all.sh
 cp build/euphonium-esp32.bin ota.bin
