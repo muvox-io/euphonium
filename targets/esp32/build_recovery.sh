4754ce489dd14212c86c387b21179bbfff44a4f5
Filip <f.grzywok@gmail.com>
2021-12-21 09:47:49 +0100

    fix: Limit files in build folder

--- targets/esp32/build_recovery.sh
+++ targets/esp32/build_recovery.sh
@@ -6,4 +6,9 @@ cd ..
 
 # copy build firmware
 rm -rf build
-cp -r recovery/build build
+mkdir -p build/bootloader
+mkdir -p build/partition_table
+cp -r recovery/build/bootloader/bootloader.bin build/bootloader/bootloader.bin
+cp -r recovery/build/partition_table/partition-table.bin build/partition_table/partition-table.bin
+cp -r recovery/build/recovery.bin build/recovery.bin
+
