81aa2baca7f688e79ab77c94b56496494a1b8382
Filip <f.grzywok@gmail.com>
2021-12-21 10:03:55 +0100

    feat: Generate sha256 in github action

--- targets/esp32/generate_full_build.sh
+++ targets/esp32/generate_full_build.sh
@@ -4,5 +4,11 @@ sh build_app.sh
 cd ../../euphonium/scripts
 tar -cvf scripts.tar *
 cd ../../targets/esp32
-cp ../../euphonium/scripts/scripts.tar build/scripts.tar
 
+# package scripts
+cp ../../euphonium/scripts/scripts.tar scripts.tar
+
+tar -czvf euphonium-$EUPH_VER.tar.gz build/*.bin build/partition_table/partition-table.bin build/bootloader/bootloader.bin
+cp build/euphonium-esp32.bin ota.bin
+sha256sum ota.bin | cut -d " " -f 1 > ota_checksum.txt
+sha256sum scripts.tar | cut -d " " -f 1 > scripts_checksum.txt
