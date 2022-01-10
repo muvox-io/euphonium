7d568728275baed459eb2d781942be5a1e90963e
Filip <f.grzywok@gmail.com>
2022-01-10 23:05:03 +0100

    feat: Make gh action that builds webide

--- web-ide/vite.config.ts
+++ web-ide/vite.config.ts
@@ -2,6 +2,22 @@ import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
+import alias from '@rollup/plugin-alias';
+
 export default defineConfig({
-  plugins: [preact()]
+  plugins: [preact()],
+  // build: {
+  //   rollupOptions: {
+  //     plugins: [
+  //       alias({
+  //         entries: [
+  //           { find: 'react', replacement: 'preact/compat' },
+  //           { find: 'react-dom/test-utils', replacement: 'preact/test-utils' },
+  //           { find: 'react-dom', replacement: 'preact/compat' },
+  //           { find: 'react/jsx-runtime', replacement: 'preact/jsx-runtime' }
+  //         ]
+  //       })
+  //     ]
+  //   }
+  // }
 })
