86370e1b6fe7126515015c1f8158ee827460be48
Filip <f.grzywok@gmail.com>
2022-03-11 00:37:55 +0100

    feat: Sidebar rework

--- web/src/components/SideBar/index.tsx
+++ web/src/components/SideBar/index.tsx
@@ -1,20 +1,37 @@
-import { Link } from "preact-router/match";
+import { Link, Match } from "preact-router/match";
 import { useState, useEffect } from "preact/hooks";
 import { PluginEntry, PluginEntryType } from "../../api/euphonium/models";
 import { getPlugins } from "../../api/euphonium/plugins";
 import Icon from "../Icon";
 import SelectItem from "../SelectItem";
 
-const SideBarItem = ({ displayName = "", name = "", type = ""}) => {
+const PluginIconMap = {
+  'dashboard': 'home',
+  'webradio': 'radio',
+  'plugin': 'playlist',
+  'general': 'settings',
+  'dac': 'dac_settings'
+} as { [key: string]: string };
+
+const SideBarItem = ({ displayName = "", name = "", type = "" }) => {
+  const hrefUrl = type == "app" ? `/web/apps/${name}` : `/web/plugin/${name}`;
+
   return (
-    <SelectItem>
-      <Link
-        activeClassName={""}
-        href={type == "app" ? `/web/apps/${name}` : `/web/plugin/${name}`}
-      >
-        <div className={""}>{displayName}</div>
-      </Link>
-    </SelectItem>
+    <Link
+      activeClassName={""}
+      href={hrefUrl}
+    >
+      <Match path={hrefUrl}>{({ matches, path, url }: any) =>
+      (<div class={`text-m flex flex-row items-center ${ matches ? 'text-app-accent' : 'text-app-text-secondary ' }`}>
+        <span class="text-2xl -ml-2 mr-1"><Icon name={PluginIconMap[name] ?? (PluginIconMap[type] ?? 'home')} /></span>
+
+        <span class="font-thin">{displayName}</span>
+      </div>)}
+      </Match>
+    </Link>
+
+
+    // </SelectItem>
   );
 };
 
@@ -27,7 +44,7 @@ type CategoryProps = {
 const SideBarCategory = ({ plugins, filterType, header }: CategoryProps) => {
   return (
     <div className="flex flex-col space-y-3">
-      <div className="-mt-1 text-lg text-app-text-secondary md:text-sm">{header}</div>
+      <div className="mt-3 text-lg text-app-text-secondary md:text-sm font-semibold">{header}</div>
 
       {plugins
         .filter((e) => e.type == filterType)
@@ -38,38 +55,40 @@ const SideBarCategory = ({ plugins, filterType, header }: CategoryProps) => {
   );
 };
 
-export default ({ version = "", theme = "", onThemeChange = () => {} }) => {
+export default ({ version = "", theme = "", onThemeChange = () => { } }) => {
   const [plugins, setPlugins] = useState<PluginEntry[]>([]);
 
   useEffect(() => {
     getPlugins().then((data) => {
-      setPlugins(data);
+      setPlugins([...data, { name: "general", displayName: "General", type: PluginEntryType.System }, { name: "dac", displayName: "DAC Settings", type: PluginEntryType.System }]);
     });
   }, []);
 
   return (
     <div className="flex align-start relative md:w-[220px] md:min-w-[220px] flex-col bg-app-primary p-8 md:p-4 h-screen text-m space-y-2 overflow-y-auto">
-      <div className="text-3xl md:text-2xl">Euphonium 🎺</div>
+      <div className="text-3xl md:text-2xl">Euphonium</div>
+      <div className="text-xs md:text-m text-app-text-secondary pb-3">tiny audio platform</div>
+      <SideBarItem displayName="Dashboard" name="home"></SideBarItem>
       <SideBarCategory
         plugins={plugins}
         filterType={PluginEntryType.App}
-        header="available apps"
+        header="Applications"
       />
       <SideBarCategory
         plugins={plugins}
         filterType={PluginEntryType.System}
-        header="system configuration"
+        header="System configuration"
       />
       <SideBarCategory
         plugins={plugins}
         filterType={PluginEntryType.Plugin}
-        header="plugin configuration"
+        header="Plugin configuration"
       />
 
 
-    <div class="absolute bottom-4 left-4 text-xs">{version}</div>
-      <div onClick={(v) => onThemeChange() } class="absolute bg-app-secondary rounded-full w-8 h-8 right-4 bottom-4 flex text-xl items-center justify-center cursor-pointer">
-        <Icon name="moon"/>
+      <div class="absolute bottom-4 left-4 text-xs">{version}</div>
+      <div onClick={(v) => onThemeChange()} class="absolute bg-app-secondary rounded-full w-8 h-8 right-4 bottom-4 flex text-xl items-center justify-center cursor-pointer">
+        <Icon name="moon" />
       </div>
     </div>
   );
