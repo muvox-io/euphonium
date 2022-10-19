f6421730ee309076cc27e472e5c4f26561b76d35
Filip Grzywok <f.grzywok@gmail.com>
2022-10-19 23:29:36 +0200

    feat(build): Use nix-shell on linux

--- nix/esp-idf/toolchain.nix
+++ nix/esp-idf/toolchain.nix
@@ -1,7 +1,28 @@
 { stdenv, lib, fetchurl, makeWrapper, buildFHSUserEnv }:
 
 let
-  test = "";
+  platforms = {
+    "x86_64-linux" = "linux-amd64";
+    "aarch64-linux" = "linux-arm64";
+    "armv7l-unknown-linux" = "linux-armel";
+    "armv7-unknown-linux" = "linux-armhf";
+    "i686-linux" = "linux-i686";
+    "x86_64-darwin" = "macos";
+    "aarch64-darwin" = "macos-arm64";
+    "i686-windows" = "win32";
+    "x86_64-windows" = "win64";
+  };
+  platformHash = {
+    "linux-amd64" = "sha256-161wxgfhln82788717m0wv9acvvp1mbh39i9m3fbn9hqqkp62873";
+    "linux-arm64" = "sha256-04py5c277d563vf04934xcdjbyq6rcdwaz3fh31c6fzvmbfckghz";
+    "linux-armel" = "sha256-12qgd7v6yhvcp9vrrv0knys1lcl40jmc1kn6gsqhvz4n8khjdfvg";
+    "linux-armhf" = "sha256-04hv96gaarwhqa8n36pjx3mz1mqi27kxa6npmz3jljmhlidqhpdh";
+    "linux-i686" = "sha256-1jvf58p96w7kd573dhh1qv8hzl0bmidq6ai1c4h7l80qn82y1y2x";
+    "macos" = "sha256-0rkj8jgx8l0bvqqf1j1r4q5w6ccnwlb2ypaizl6j47kj0ch9y0m3";
+    "macos-arm64" = "sha256-sUGJdy1wqWgTiV//dzHQ8v7AyCXPwC4ALW2RoMxLax0";
+  };
+  platform = platforms.${stdenv.system};
+  toolchainHash = platformHash.${platform};
 in
 
 stdenv.mkDerivation rec {
@@ -9,8 +30,8 @@ stdenv.mkDerivation rec {
   version = "2021r2-patch5";
 
   src = fetchurl {
-    url = "https://github.com/espressif/crosstool-NG/releases/download/esp-${version}/xtensa-esp32-elf-gcc8_4_0-esp-${version}-macos-arm64.tar.gz";
-    hash = "sha256-sUGJdy1wqWgTiV//dzHQ8v7AyCXPwC4ALW2RoMxLax0";
+    url = "https://github.com/espressif/crosstool-NG/releases/download/esp-${version}/xtensa-esp32-elf-gcc8_4_0-esp-${version}-${platform}.tar.gz";
+    hash = toolchainHash;
   };
 
   buildInputs = [ makeWrapper ];
--- nix/shell.nix
+++ nix/shell.nix
@@ -1,9 +1,8 @@
 {}:
 
 let
-    pkgs = import(builtins.fetchTarball "channel:nixpkgs-22.05-darwin") {};
-    inherit (pkgs.darwin.apple_sdk.frameworks) CoreServices ApplicationServices Security;
-    darwinPackages = [ CoreServices ApplicationServices Security ];
+    pkgs = import(builtins.fetchTarball "channel:nixos-22.05") {};
+    darwinPackages =  if pkgs.stdenv.isDarwin then [ pkgs.darwin.apple_sdk.frameworks.CoreServices pkgs.darwin.apple_sdk.frameworks.ApplicationServices pkgs.darwin.apple_sdk.frameworks.Security ] else [];
 in
 pkgs.mkShell {
     buildInputs = with pkgs; [
