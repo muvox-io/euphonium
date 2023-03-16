ac2ec5c94b56bf510fae11450a76df48eb1dea69
alufers <alufers@wp.pl>
2023-03-16 17:48:18 +0100

    Remove console.logs

--- src/web-ui/src/utils/useAPI.hook.ts
+++ src/web-ui/src/utils/useAPI.hook.ts
@@ -6,7 +6,6 @@ export default function useAPI<A>(api: new (acc: APIAccessor) => A): A {
   if (accessor.apiCache[api.name]) {
     return accessor.apiCache[api.name];
   }
-  console.log( accessor.apiCache)
   const apiInstance = new api(accessor);
   accessor.apiCache[api.name] = apiInstance;
   return apiInstance;
