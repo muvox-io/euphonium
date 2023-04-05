e26b38e61f5fac0a5310a040414889647894d6b1
alufers <alufers@wp.pl>
2023-04-05 18:45:55 +0200

    Add default radio fetching

--- src/frontend/src/api/radiobrowser/api.ts
+++ src/frontend/src/api/radiobrowser/api.ts
@@ -2,10 +2,27 @@ import { Station } from "./models";
 
 let radioBrowserUrl = "https://radio-browser.gkindustries.pl";
 
-const getStationsByName = async (name: string, limit: number, offset: number): Promise<Station[]> => {
-    return fetch(`${radioBrowserUrl}/json/stations/byname/${name}?offset=${offset}&limit=${limit}&hidebroken=true`)
-        .then(response => response.json())
-        .catch(error => console.error(error));
-}
+const getStationsByName = async (
+  name: string,
+  limit: number,
+  offset: number
+): Promise<Station[]> => {
+  return fetch(
+    `${radioBrowserUrl}/json/stations/byname/${name}?offset=${offset}&limit=${limit}&hidebroken=true`
+  )
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+};
 
-export { getStationsByName }
+const getTopStations = async (
+  limit: number,
+  offset: number
+): Promise<Station[]> => {
+  return fetch(
+    `${radioBrowserUrl}/json/stations?offset=${offset}&limit=${limit}`
+  )
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+};
+
+export { getStationsByName, getTopStations };
