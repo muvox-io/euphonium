099cfd06be2b22281b2fbeaa9ab0c015941ecfeb
alufers <alufers@wp.pl>
2023-07-25 18:15:37 +0200

    fix: NumberField default values not working

--- src/frontend/src/components/FormFields/fields/NumberField.tsx
+++ src/frontend/src/components/FormFields/fields/NumberField.tsx
@@ -1,32 +1,48 @@
-
-
 import { useDispatch, useSelector } from "react-redux";
 import { ConfigurationNumberField } from "../../../api/euphonium/plugins/models";
-import { onFieldChanged, pluginStateSelector } from "../../../redux/reducers/pluginConfigurationsReducer";
+import {
+  onFieldChanged,
+  pluginStateSelector,
+} from "../../../redux/reducers/pluginConfigurationsReducer";
 import Input from "../../ui/Input";
 import { FieldProps } from "./FieldProps";
 
-export default function NumberField({ field, pluginName }: FieldProps<ConfigurationNumberField >) {
+export default function NumberField({
+  field,
+  pluginName,
+}: FieldProps<ConfigurationNumberField>) {
   const pluginState = useSelector(pluginStateSelector(pluginName));
   const dispatch = useDispatch();
+  const val =
+    pluginState[field.stateKey] == null
+      ? field.default
+      : pluginState[field.stateKey];
   return (
     <Input
       type="number"
       tooltip={field.label}
-      value={pluginState[field.stateKey]}
+      value={val.toString()}
       width={90}
-      onChange={(str) => dispatch(onFieldChanged({
-        pluginName,
-        key: field.stateKey,
-        value: parseFloat(str),
-        debounce: true
-      }) as any)}
-      onBlur={() => dispatch(onFieldChanged({
-        pluginName,
-        key: field.stateKey,
-        value: pluginState[field.stateKey],
-        debounce: false
-      }) as any)}
+      onChange={(str) =>
+        dispatch(
+          onFieldChanged({
+            pluginName,
+            key: field.stateKey,
+            value: parseFloat(str),
+            debounce: true,
+          }) as any
+        )
+      }
+      onBlur={() =>
+        dispatch(
+          onFieldChanged({
+            pluginName,
+            key: field.stateKey,
+            value: val,
+            debounce: false,
+          }) as any
+        )
+      }
     />
   );
 }
