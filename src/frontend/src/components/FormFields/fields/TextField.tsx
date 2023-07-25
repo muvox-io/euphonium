ac3820d4048c51c89a71fe666ffe00ab7a426e94
alufers <alufers@wp.pl>
2023-07-25 18:17:50 +0200

    Fix default value handling in TextField

--- src/frontend/src/components/FormFields/fields/TextField.tsx
+++ src/frontend/src/components/FormFields/fields/TextField.tsx
@@ -1,28 +1,46 @@
 import { useDispatch, useSelector } from "react-redux";
 import { ConfigurationFieldText } from "../../../api/euphonium/plugins/models";
-import { onFieldChanged, pluginStateSelector } from "../../../redux/reducers/pluginConfigurationsReducer";
+import {
+  onFieldChanged,
+  pluginStateSelector,
+} from "../../../redux/reducers/pluginConfigurationsReducer";
 import Input from "../../ui/Input";
 import { FieldProps } from "./FieldProps";
 
-export default function TextField({ field, pluginName }: FieldProps<ConfigurationFieldText>) {
+export default function TextField({
+  field,
+  pluginName,
+}: FieldProps<ConfigurationFieldText>) {
   const pluginState = useSelector(pluginStateSelector(pluginName));
   const dispatch = useDispatch();
+  const val =
+    pluginState[field.stateKey] == null
+      ? field.default
+      : pluginState[field.stateKey];
   return (
     <Input
       tooltip={field.label}
-      value={pluginState[field.stateKey]}
-      onChange={(str) => dispatch(onFieldChanged({
-        pluginName,
-        key: field.stateKey,
-        value: str,
-        debounce: true
-      }) as any)}
-      onBlur={() => dispatch(onFieldChanged({
-        pluginName,
-        key: field.stateKey,
-        value: pluginState[field.stateKey],
-        debounce: false
-      }) as any)}
+      value={val}
+      onChange={(str) =>
+        dispatch(
+          onFieldChanged({
+            pluginName,
+            key: field.stateKey,
+            value: str,
+            debounce: true,
+          }) as any
+        )
+      }
+      onBlur={() =>
+        dispatch(
+          onFieldChanged({
+            pluginName,
+            key: field.stateKey,
+            value: val,
+            debounce: false,
+          }) as any
+        )
+      }
     />
   );
 }
